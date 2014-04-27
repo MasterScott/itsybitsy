7fef45a052f4697a55b8974b6148f2779d482071
Sarah Jamie Lewis <me@sarahjamielewis.com>
2014-04-27 16:47:41 -0700

    Cleaning up gen.sh

--- gen.sh
+++ gen.sh
@@ -4,30 +4,34 @@
 SITE_FILES="./site/"
 
 mkdir -p ./tmp/
-find $SITE_FILES -name "*.md~" | xargs rm
+find $SITE_FILES -name "*.md~" | xargs rm -f
 
 # Copy all Markdown files in site/ to tmp/
-cd $SITE_FILES
-cp -r * ../tmp/
-cd ../
+cp -r $SITE_FILES/* ./tmp/
 
 FILES=$(find ./tmp/ -name "*.md")
 
 for f in $FILES
 do
   echo "Processing $f file..."
+
+  # Each directory attempts to use its own template.
   BASE=$(basename $(dirname $f))
 
   if [ $BASE = "tmp" ]; then
 	BASE="default"
   fi
   TITLE=$(head -n 1 $f)
-  echo $TITLE
+  
   perl Markdown.pl --html4tags $f > ${f%.*}.stage.html;
   cp ./templates/$BASE.html ${f%.*}.tmp.html
+
+  # Search and replace time.
   sed -e "/\%body/r ${f%.*}.stage.html" -e "/$str/d" ${f%.*}.tmp.html > ${f%.*}.title.html
   sed "s/\%title/$TITLE/g" ${f%.*}.title.html > ${f%.*}.html 
   rm ${f%.*}.stage.html ${f%.*}.title.html ${f%.*}.tmp.html $f
 done
 
+# Clean up and rename
+rm -rf ./_site
 mv ./tmp ./_site
